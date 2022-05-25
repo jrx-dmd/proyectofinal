99cd990b32cf729c83c8d2bc2131665c48028238
jrx-dmd <david.moya@jaraxa.com>
2022-05-25 13:21:53 +0200

    add archives

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+
+  cloud {
+    organization = "jrx-dmd"
+
+    workspaces {
+      name = "david"
+    }
+  }
+}
+
+variable "digitalocean_token" {}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = "${var.digitalocean_token}"
+}
